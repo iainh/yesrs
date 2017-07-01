07bcf0ae3087b6c7d7b5c511f45cbbc01017c7d5
Iain H <iain@spiralpoint.org>
2017-07-01 14:14:57 -0400

    Simple version of 'yes' written in rust.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+#[macro_use]
+extern crate clap;
+
+use clap::{App, Arg, ArgMatches};
+
+fn parse_args() -> ArgMatches<'static> {
+    App::new("yes")
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about("Repeatedly output a line with all specified STRING(s), or 'y'.")
+        .arg(Arg::with_name("STRING")
+            .index(1))
+        .get_matches()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = parse_args();
+
+    let value = args.value_of("STRING");
+    match value {
+        Some(s) => {
+            loop {
+                println!("{}", s);
+            }
+        }
+        None => {
+            loop {
+                println!("y");
+            }
+        }
+    }
 }
